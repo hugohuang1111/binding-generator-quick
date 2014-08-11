05ad472c0a4752567bd294d9ae0df0f36cc425ac
SunLightJuly <ygqy8181@qq.com>
2014-08-11 18:39:31 +0800

    cleanup event classed

--- for-cocos2dxV3/tolua/genbindings.py
+++ for-cocos2dxV3/tolua/genbindings.py
@@ -148,6 +148,7 @@ def main():
         #             'cocos2dx_spine.ini' : ('cocos2dx_spine', 'lua_cocos2dx_spine_auto'), \
         #             'cocos2dx_physics.ini' : ('cocos2dx_physics', 'lua_cocos2dx_physics_auto'), \
         #             'cocos2dx_extra.ini' : ('cocos2dx_extra', 'lua_cocos2dx_extra_auto'), \
+        #             'cocos2dx_studio_Armature.ini' : ('cocos2dx_studio', 'lua_cocos2dx_studio_auto'), \
         #             }
         target = 'lua'
         generator_py = '%s/generator.py' % cxx_generator_root
